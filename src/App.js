c2c2c445f22ba5cd617f9cae3ce286fe31c5b250
Hiba <hibat64@gmail.com>
2020-08-02 09:32:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Imgsrc from "./imageInSrc.jpg";
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Hiba</h1>
+        <br />
+        <img src={Imgsrc} /> <br />
+        <img src="/imageInPublic.jpg" />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="./myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
-
 export default App;
